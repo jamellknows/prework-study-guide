6b68239c280d3a793693433e3ab878257b914066
jamellknows <jamellsamuels@googlemail.com>
2022-11-02 16:19:51 +0000

    <your message here>

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(var ind = 0; ind < topics.length; ind++){
+    console.log(topics[ind])
+}
+var topic = "HTML";
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
